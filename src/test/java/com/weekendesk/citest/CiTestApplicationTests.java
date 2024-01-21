a3885868f17d92cdaecd25595a6faca9f71d5019
javier-tercedor <javier.tercedor@weekendesk.fr>
2024-01-21 11:49:24 +0000

    first commit

---
+++ src/test/java/com/weekendesk/citest/CiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weekendesk.citest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
